621357376f97ee8beab18476700cbbdec9633eb1
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-06 21:36:22 +0300

    feat: add js to burger menu

--- dist/index.579125c3.js
+++ dist/index.579125c3.js
@@ -1,2 +1,21 @@
+addEventListener("DOMContentLoaded", ()=>{
+    const menu = document.querySelector(".menu__body");
+    const menuBtn = document.querySelector(".menu__icon");
+    const body = document.body;
+    if (menu && menuBtn) {
+        menuBtn.addEventListener("click", ()=>{
+            menu.classList.toggle("active");
+            menuBtn.classList.toggle("active");
+            body.classList.toggle("lock");
+        });
+        menu.querySelectorAll(".menu__link").forEach((link)=>{
+            link.addEventListener("click", ()=>{
+                menu.classList.remove("active");
+                menuBtn.classList.remove("active");
+                body.classList.remove("lock");
+            });
+        });
+    }
+});
 
 //# sourceMappingURL=index.579125c3.js.map
