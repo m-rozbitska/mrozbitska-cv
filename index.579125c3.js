b45273a64449756189ddc0e856e6fc5aa07ca572
Mariia Rozbitska <melissa180lily@gmail.com>
2023-10-17 13:09:42 +0300

    change js code

--- index.579125c3.js
+++ index.579125c3.js
@@ -1,21 +1,22 @@
-addEventListener("DOMContentLoaded", ()=>{
-    const menu = document.querySelector(".menu__body");
-    const menuBtn = document.querySelector(".menu__icon");
-    const body = document.body;
-    if (menu && menuBtn) {
-        menuBtn.addEventListener("click", ()=>{
-            menu.classList.toggle("active");
-            menuBtn.classList.toggle("active");
-            body.classList.toggle("lock");
-        });
-        menu.querySelectorAll(".menu__link").forEach((link)=>{
-            link.addEventListener("click", ()=>{
-                menu.classList.remove("active");
-                menuBtn.classList.remove("active");
-                body.classList.remove("lock");
-            });
-        });
-    }
+document.addEventListener("DOMContentLoaded", () => {
+	const menu = document.querySelector('.menu__body');
+	const menuBtn = document.querySelector('.menu__icon');
+	const body = document.body;
+	const menuLinks = document.querySelectorAll('.menu__link');
+
+	menuBtn.addEventListener('click', () => {
+		menu.classList.add('active');
+		menuBtn.classList.add('active');
+		body.classList.add('lock');
+	});
+
+	menuLinks.forEach(link => {
+		link.addEventListener('click', () => {
+			menu.classList.remove('active');
+			menuBtn.classList.remove('active');
+			body.classList.remove('lock');
+		});
+	});
 });
 
 //# sourceMappingURL=index.579125c3.js.map
